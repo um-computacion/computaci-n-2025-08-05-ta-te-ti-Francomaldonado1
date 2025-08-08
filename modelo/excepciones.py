6ab883ac2ee51dc33499eb7eaf2e7d60453907cc
francomaldonado1 <f.maldonado@alumno.um.edu.ar>
2025-08-08 09:33:25 -0300

    Borrando excepciones innecesarias

--- modelo/excepciones.py
+++ modelo/excepciones.py
@@ -6,21 +6,6 @@ class PosicionInvalidaException(Exception):
     def __init__(self, mensaje="Posición fuera del tablero."):
         super().__init__(mensaje)
 
-
-class FichaInvalidaException(Exception):
-    def __init__(self, mensaje="Ficha inválida. Debe ser 'X' o 'O'."):
-        super().__init__(mensaje)
-
-
-class TurnoInvalidoException(Exception):
-    def __init__(self, mensaje="No es el turno de este jugador."):
-        super().__init__(mensaje)
-
-
-class JuegoFinalizadoException(Exception):
-    def __init__(self, mensaje="El juego ya terminó."):
-        super().__init__(mensaje)
-
 class CaracterNoNumericoException(Exception):
     def __init__(self, mensaje="Debe ingresar un número. No se permiten letras ni símbolos."):
         super().__init__(mensaje)
